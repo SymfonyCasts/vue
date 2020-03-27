5f575f3bd4bb7ffbefa57d5d025e7951d651a71a
shadowc <info@matiasjose.com>
2020-03-27 17:25:16 -0300

    stich

--- assets/js/products.vue
+++ assets/js/products.vue
@@ -2,74 +2,25 @@
     <div class="container-fluid">
         <div class="row row-no-wrap">
             <aside class="col-xs-12 col-lg-3">
-                <div :class="[$style.aside, 'p-3', 'mt-5', 'mb-5']">
-                    <h5 class="text-center">
-                        Categories
-                    </h5>
-
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a
-                                class="nav-link"
-                                href="/"
-                            >All Products</a>
-                        </li>
-                    </ul>
-                </div>
+                <sidebar-component />
             </aside>
 
             <div class="col-xs-12 col-lg-9">
-                <div class="row">
-                    <h2 class="p-3">
-                        Products
-                    </h2>
-                </div>
-
-                <div class="row">
-                    <legend-component :title="legend" />
-                </div>
+                <catalog-component />
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import LegendComponent from './components/legend';
+import CatalogComponent from './components/catalog';
+import SidebarComponent from './components/sidebar';
 
 export default {
     name: 'Products',
     components: {
-        LegendComponent,
+        CatalogComponent,
+        SidebarComponent,
     },
-    data: () => ({
-        legend: 'Shipping takes 10-12 weeks, and products probably won\'t work',
-    }),
 };
 </script>
-
-<style lang="scss" module>
-@import '../scss/components/light-component';
-
-.aside {
-    @include light-component;
-    margin-top: 65px;
-
-    ul {
-        border-bottom: 1px solid $light-component-border;
-        margin-bottom: 20px;
-        padding-bottom: 10px;
-
-        li a {
-            color: #000;
-        }
-
-        li a:hover {
-            background: $blue-component-link-hover;
-        }
-
-        li a.selected {
-            background: $light-component-border;
-        }
-    }
-}
-</style>
