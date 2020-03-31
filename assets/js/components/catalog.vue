cce464ef39e851e756e11809caa627bdb4d8aaf2
shadowc <info@matiasjose.com>
2020-03-31 19:16:53 -0300

    catalog

--- assets/js/components/catalog.vue
+++ assets/js/components/catalog.vue
@@ -6,6 +6,10 @@
             </h2>
         </div>
 
+        <product-list
+            :products="products"
+        />
+
         <div class="row">
             <legend-component :title="legend" />
         </div>
@@ -13,15 +17,29 @@
 </template>
 
 <script>
+import axios from 'axios';
 import LegendComponent from '@/components/legend';
+import ProductList from '@/components/product-list';
 
 export default {
     name: 'Catalog',
     components: {
         LegendComponent,
+        ProductList,
     },
     data: () => ({
         legend: 'Shipping takes 10-12 weeks, and products probably won\'t work',
+        products: [],
     }),
+    async mounted() {
+        this.products = [];
+
+        const response = await axios({
+            method: 'get',
+            url: '/api/products',
+        });
+
+        this.products = response.data['hydra:member'];
+    },
 };
 </script>
