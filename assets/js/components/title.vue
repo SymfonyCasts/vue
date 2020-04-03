823794af2994056008360214f555be54e1993c0f
shadowc <info@matiasjose.com>
2020-04-03 20:52:08 -0300

    title component

---
+++ assets/js/components/title.vue
@@ -0,0 +1,31 @@
+<template>
+    <h6>{{ category.name }}</h6>
+</template>
+
+<script>
+export default {
+    name: 'Title',
+    props: {
+        currentCategoryId: {
+            type: Number,
+            default: null,
+        },
+        categories: {
+            type: Array,
+            default: () => ([]),
+        },
+    },
+    computed: {
+        category() {
+            if (typeof this.currentCategoryId === 'undefined') {
+                return {
+                    id: null,
+                    name: 'All Products',
+                };
+            }
+
+            return this.categories.find((cat) => (cat.id === this.currentCategoryId));
+        },
+    },
+};
+</script>
