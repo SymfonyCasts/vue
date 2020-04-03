11f16d20dbe37e6ebd590506758f4541e183367f
shadowc <info@matiasjose.com>
2020-04-03 20:01:06 -0300

    remove selected class

--- assets/js/products.vue
+++ assets/js/products.vue
@@ -72,10 +72,6 @@ export default {
         li a:hover {
             background: $blue-component-link-hover;
         }
-
-        li a.selected {
-            background: $light-component-border;
-        }
     }
 }
 </style>
