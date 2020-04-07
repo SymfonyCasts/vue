f13f13b2fb23b82a0322297abb82c5bd045eacc3
shadowc <info@matiasjose.com>
2020-04-07 15:01:16 -0300

    Remove function

--- assets/js/components/catalog.vue
+++ assets/js/components/catalog.vue
@@ -93,30 +93,6 @@ export default {
                 this.loading = false;
             }
         },
-
-        /**
-         * Generates the URL to call for a list of products by category or search term
-         *
-         * @param {string} searchTerm
-         * @return {string}
-         */
-        generateListingUrl(searchTerm) {
-            let url = '/api/products';
-
-            if (this.currentCategoryId || searchTerm !== '') {
-                url += '?';
-
-                if (this.currentCategoryId && searchTerm === '') {
-                    url += `category=${this.currentCategoryId}`;
-                } else if (!this.currentCategoryId && searchTerm !== '') {
-                    url += `name=${searchTerm}`;
-                } else {
-                    url += `category=${this.currentCategoryId}&name=${searchTerm}`;
-                }
-            }
-
-            return url;
-        },
     },
 };
 </script>
