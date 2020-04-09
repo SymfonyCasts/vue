2e5eae50fa46df78ccbfdafa81ae2128ef8ccb3d
shadowc <info@matiasjose.com>
2020-04-09 18:23:15 -0300

    blank

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 
-use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
