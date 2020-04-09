a29125b1bf0b9b065983c2eb1b1f6d3614a3a868
shadowc <info@matiasjose.com>
2020-04-08 21:14:58 -0300

    import

--- assets/js/components/product-list/index.vue
+++ assets/js/components/product-list/index.vue
@@ -1,17 +1,21 @@
 <template>
     <div :class="$style.component">
-        <div
+        <product-card
             v-for="product in products"
             :key="product['@id']"
-        >
-            {{ product.name }}
-        </div>
+            :item="product"
+        />
     </div>
 </template>
 
 <script>
+import ProductCard from '@/components/product-list/product-card';
+
 export default {
     name: 'ProductList',
+    components: {
+        ProductCard,
+    },
     props: {
         products: {
             type: Array,
