b2ed50b8ad2ab1cb19f9bd17a0b76607cfc2f53d
shadowc <info@matiasjose.com>
2020-04-10 17:11:10 -0300

    Step 79: Title Component: Create Component

---
+++ assets/js/components/title.vue
@@ -0,0 +1,44 @@
+<template>
+    <div :class="$style.component">
+        <h6 class="ml-2">
+            {{ category.name }}
+        </h6>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Title',
+    props: {
+        currentCategoryId: {
+            type: Number,
+            default: null,
+        },
+        categories: {
+            type: Array,
+            default: () => ([]),
+        },
+    },
+    computed: {
+        category() {
+            if (this.currentCategoryId === null) {
+                return {
+                    id: null,
+                    name: 'All Products',
+                };
+            }
+
+            return this.categories.find((cat) => (cat.id === this.currentCategoryId));
+        },
+    },
+};
+</script>
+
+<style lang="scss" module>
+.component {
+    h6 {
+        font-size: 1.7rem;
+        margin-left: -6px !important;
+    }
+}
+</style>
