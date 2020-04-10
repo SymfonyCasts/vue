b8d5daee9a4f35894020f1cc9a3a693e624d5be6
shadowc <info@matiasjose.com>
2020-04-10 17:08:23 -0300

    Step 36: Products Listing: First Product List

---
+++ assets/js/components/product-list/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div :class="$style.component">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductList',
+    props: {
+        products: {
+            type: Array,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style lang="scss" module>
+.component {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    margin-top: 25px;
+}
+</style>
