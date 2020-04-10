fa1a7ea48ea02c60cd751f2609f3b80b986cd1be
shadowc <info@matiasjose.com>
2020-04-10 17:12:28 -0300

    Step 99: Final Search Bar: generateListingUrl helper skeleton

---
+++ assets/js/helpers/generate-listing-url.js
@@ -0,0 +1,11 @@
+/**
+ * Generates the URL to call for a list of products by category or search term
+ *
+ * @param {number|null} category
+ * @param {string} searchTerm
+ * @return {string}
+ */
+const generateListingUrl = (category, searchTerm) => {
+};
+
+export default generateListingUrl;
