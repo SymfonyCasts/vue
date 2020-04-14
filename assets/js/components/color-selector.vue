92deee9be32d5f872d0312d08d378019422e0974
shadowc <info@matiasjose.com>
2020-04-14 02:58:02 -0300

    color

---
+++ assets/js/components/color-selector.vue
@@ -0,0 +1,21 @@
+
+<script>
+import colorsService from '@/services/colors';
+
+export default {
+    name: 'ColorSelector',
+    data: () => ({
+        colors: [],
+        selectedIndex: null,
+    }),
+    async created() {
+        try {
+            const response = await colorsService.getColors();
+
+            this.colors = response.data['hydra:member'];
+        } catch (e) {
+            this.colors = [];
+        }
+    },
+};
+</script>
