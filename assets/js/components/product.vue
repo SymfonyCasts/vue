207257ac1f93387f8ce286e8b4f43692815ca798
shadowc <info@matiasjose.com>
2020-04-15 16:49:45 -0300

    Step 124: Linking Products: Copy props to Product component

--- assets/js/components/product.vue
+++ assets/js/components/product.vue
@@ -7,5 +7,19 @@
 <script>
 export default {
     name: 'Product',
+    props: {
+        currentCategoryId: {
+            type: Number,
+            default: null,
+        },
+        currentProductId: {
+            type: Number,
+            default: null,
+        },
+        categories: {
+            type: Array,
+            default: () => ([]),
+        },
+    },
 };
 </script>
