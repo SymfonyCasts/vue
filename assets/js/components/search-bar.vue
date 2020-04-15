aa64da4a9b15fab3bb2aa4ab1f4662162528ca6c
shadowc <info@matiasjose.com>
2020-04-15 16:47:00 -0300

    Step 82: First Search Bar: Create Component

---
+++ assets/js/components/search-bar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div :class="$style.component">
+        <input
+            v-model="searchTerm"
+            class="form-control"
+            placeholder="Search products..."
+            type="text"
+        >
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar',
+    data: () => ({
+        searchTerm: '',
+    }),
+};
+</script>
+
+<style lang="scss" module>
+@import '~styles/variables/colors.scss';
+
+.component {
+    width: 400px;
+}
+</style>
