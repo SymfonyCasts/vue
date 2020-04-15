ff83fa8aebbb3312f784601de76c79ce96b0d0ab
shadowc <info@matiasjose.com>
2020-04-14 22:22:51 -0300

    encore

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,7 @@ Encore
      */
     .addEntry('app', './assets/js/app.js')
     .addEntry('products', './assets/js/products.js')
+    .addEntry('shopping-cart', './assets/js/shopping-cart.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
