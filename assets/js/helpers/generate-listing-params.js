89e313db4ab93a8fc2ef5db0a072864825e452ab
shadowc <info@matiasjose.com>
2020-04-16 17:45:47 -0300

    fix helper

---
+++ assets/js/helpers/generate-listing-params.js
@@ -0,0 +1,12 @@
+/**
+ * Generates the params for the URL to call for a list of products
+ * by category or search term
+ *
+ * @param {number|null} category
+ * @param {string} searchTerm
+ * @return {object}
+ */
+const generateListingParams = (category, searchTerm) => {
+};
+
+export default generateListingParams;
