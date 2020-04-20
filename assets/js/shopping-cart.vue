dbd0689d7d927b21a1185b8c9f19b3e7349c94c9
shadowc <info@matiasjose.com>
2020-04-20 14:20:50 -0300

    Step 178: New Entry: Create main component

---
+++ assets/js/shopping-cart.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container-fluid">
+        <div class="row row-no-wrap">
+            <aside class="col-xs-12 col-lg-3" />
+
+            <div class="col-xs-12 col-lg-9">
+                // TODO: List the products in the cart here
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ShoppingCart',
+};
+</script>
+
+<style lang="scss" module>
+</style>
