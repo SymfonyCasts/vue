a8eb13afa1defd00c2fd1390dd07d36e63c215b0
shadowc <info@matiasjose.com>
2020-04-20 14:14:54 -0300

    Step 85: First Search Bar: Create Skeleton component

---
+++ assets/js/components/search-bar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div :class="$style.component">
+        <input
+            class="form-control"
+            placeholder="Search products..."
+            type="text"
+        >
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar',
+};
+</script>
+
+<style lang="scss" module>
+.component {
+    width: 400px;
+}
+</style>
