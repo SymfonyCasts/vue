b729e8abf82d6415c2c8e72bd10fe847a5e3777a
shadowc <info@matiasjose.com>
2020-04-20 14:12:17 -0300

    Step 44: Loading State: Create Loading Component

---
+++ assets/js/components/loading.vue
@@ -0,0 +1,18 @@
+<template>
+    <h4 :class="$style.component">
+        Loading...
+    </h4>
+</template>
+
+<script>
+export default {
+    name: 'Loading',
+};
+</script>
+
+<style lang="scss" module>
+.component {
+    background: url('../../images/loading.gif') no-repeat left center;
+    padding: 0 0 4px 50px;
+}
+</style>
