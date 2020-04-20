3cf23427ef5b99b878c7dea6c31698b879d7bb17
shadowc <info@matiasjose.com>
2020-04-20 14:18:05 -0300

    Step 136: Basic Product Page: Add basic stuff to component

--- assets/js/components/product.vue
+++ assets/js/components/product.vue
@@ -1,12 +1,22 @@
 <template>
-    <div>
-        // Product goes here
+    <div class="row">
+        <div :class="$style['top-bar']">
+            <title-component />
+        </div>
+
+        TODO: Show product here!
     </div>
 </template>
 
 <script>
+import productsService from '@/services/products';
+import TitleComponent from '@/components/title';
+
 export default {
     name: 'Product',
+    components: {
+        TitleComponent,
+    },
     props: {
         currentProductId: {
             type: Number,
@@ -16,6 +26,20 @@ export default {
             type: Array,
             default: () => ([]),
         },
+    },
+    data: () => ({
+        product: null,
+        loading: true,
+    }),
+    async created() {
+        try {
+            const response = await productsService.getProduct(this.currentProductId);
+
+            this.product = response.data;
+            this.loading = false;
+        } catch (e) {
+            this.loading = false;
+        }
     },
 };
 </script>
