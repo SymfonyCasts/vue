f412ff26eab8a1d5c3835c20ffc1f10745238e5b
shadowc <info@matiasjose.com>
2020-04-22 16:16:37 -0300

    paste the form from checkout

---
+++ assets/js/components/checkout/form-input.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="form-row">
+        <label
+            for="customerName"
+            class="col-form-label"
+        >
+            Name:
+        </label>
+        <input
+            id="customerName"
+            v-model.trim="form.customerName"
+            type="text"
+            :class="{
+                'is-invalid': !isFieldValid('customerName'),
+                'form-control': true,
+            }"
+        >
+        <span
+            v-show="isFieldValid('customerName')"
+            class="invalid-feedback"
+        >
+            {{ validation.customerName }}
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FormInput',
+};
+</script>
