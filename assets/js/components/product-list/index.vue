1939e0c0895452fc9c8a3d502a602cfb2529b0c4
shadowc <info@matiasjose.com>
2020-04-23 16:53:23 -0300

    Step 63: Better Loading: Add prop to Product List

--- assets/js/components/product-list/index.vue
+++ assets/js/components/product-list/index.vue
@@ -22,6 +22,10 @@ export default {
         ProductCard,
     },
     props: {
+        loading: {
+            type: Boolean,
+            required: true,
+        },
         products: {
             type: Array,
             required: true,
