edcf7d637963a0fb06a10a67311cac9185f27f8c
shadowc <info@matiasjose.com>
2020-04-23 16:55:29 -0300

    Step 97: First Search Bar: Add searchTerm data to catalog

--- assets/js/components/catalog.vue
+++ assets/js/components/catalog.vue
@@ -50,6 +50,7 @@ export default {
     },
     data: () => ({
         products: [],
+        searchTerm: '',
         loading: true,
         legend: 'Shipping takes 10-12 weeks, and products probably won\'t work',
     }),
