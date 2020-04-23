5ecfc2d7e9906754addda49b23e13b76037c3888
shadowc <info@matiasjose.com>
2020-04-23 17:03:19 -0300

    Step 217: Clear and Checkout: First component

---
+++ assets/js/components/shopping-cart/actions.vue
@@ -0,0 +1,34 @@
+<template>
+    <div
+        :class="$style.component"
+    >
+        <button
+            class="btn btn-info btn-sm"
+        >
+            Clear Shopping Cart
+        </button>
+
+        <button
+            class="btn btn-info btn-sm"
+        >
+            Check Out!
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CheckoutActions',
+};
+</script>
+
+<style lang="scss" module>
+.component {
+    display: flex;
+    justify-content: flex-end;
+
+    button {
+        margin-right: 20px;
+    }
+}
+</style>
