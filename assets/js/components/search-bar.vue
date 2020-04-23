4c13182f30510f75bb6b4276bfa9ef63222a09c5
shadowc <info@matiasjose.com>
2020-04-23 16:56:47 -0300

    Step 117: V-Input Search Bar: Fix eraseSearchTerm

--- assets/js/components/search-bar.vue
+++ assets/js/components/search-bar.vue
@@ -31,6 +31,7 @@ export default {
          */
         eraseSearchTerm() {
             this.searchTerm = '';
+            this.$emit('search-products', { term: this.searchTerm });
         },
 
         /**
