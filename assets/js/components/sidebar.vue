0e61115fcdb910d53e2da9400ba7cdb6f0e5805b
shadowc <info@matiasjose.com>
2020-04-23 16:54:21 -0300

    Step 78: Optimized Load Times: Add selected class in scss

--- assets/js/components/sidebar.vue
+++ assets/js/components/sidebar.vue
@@ -84,6 +84,10 @@ export default {
         li a:hover {
             background: $blue-component-link-hover;
         }
+
+        li a.selected {
+            background: $light-component-border;
+        }
     }
 
     .buttons {
