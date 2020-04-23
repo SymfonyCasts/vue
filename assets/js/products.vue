d7d1408709feb7cfa31ee7d4e86eddf7bbddce71
shadowc <info@matiasjose.com>
2020-04-23 16:49:41 -0300

    Step 6: First Vue Component: Create Component

---
+++ assets/js/products.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container-fluid">
+        <div class="row row-no-wrap">
+            <aside class="col-xs-12 col-lg-3">
+                <div class="p-3 mb-5">
+                    <h5 class="text-center">
+                        Categories
+                    </h5>
+
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a
+                                class="nav-link"
+                                href="/"
+                            >All Products</a>
+                        </li>
+                        <li class="nav-item">
+                            <a
+                                class="nav-link"
+                                href="#"
+                            >Category A</a>
+                        </li>
+                        <li class="nav-item">
+                            <a
+                                class="nav-link"
+                                href="#"
+                            >Category B</a>
+                        </li>
+                    </ul>
+                </div>
+            </aside>
+
+            <div class="col-xs-12 col-lg-9">
+                <div class="row">
+                    <h2 class="p-3">
+                        Products
+                    </h2>
+                </div>
+
+                <div class="row">
+                    <span class="p-3">
+                        Shipping takes 10-12 weeks, and products probably won't work
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Products',
+};
+</script>
