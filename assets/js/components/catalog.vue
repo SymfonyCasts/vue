311095f7ba80941bdda1379f4de364dbee07856d
shadowc <info@matiasjose.com>
2020-04-23 16:50:17 -0300

    Step 15: SideBar: Create The Catalog Component

---
+++ assets/js/components/catalog.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div class="row">
+            <h2 class="p-3">
+                Products
+            </h2>
+        </div>
+
+        <div class="row">
+            <legend-component :title="legend" />
+        </div>
+    </div>
+</template>
+
+<script>
+import LegendComponent from './legend';
+
+export default {
+    name: 'Catalog',
+    components: {
+        LegendComponent,
+    },
+    data: () => ({
+        legend: 'Shipping takes 10-12 weeks, and products probably won\'t work',
+    }),
+};
+</script>
