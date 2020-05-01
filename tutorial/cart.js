ef1732e35a37fb32f0ea32cfde089ebcca4fcb91
shadowc <info@matiasjose.com>
2020-05-01 16:53:35 -0300

    Add cart service to our starting point

---
+++ tutorial/cart.js
@@ -0,0 +1,121 @@
+const cart = {
+    /**
+     * Gets the list of all shopping cart items in an array of objects containing:
+     * - productId
+     * - colorId
+     * - qty
+     *
+     * @return {object}
+     */
+    getItems() {
+        return cart.getStorage();
+    },
+
+    /**
+     * Adds a product into the cart, providing a product id, an optional color id and the quantity
+     *
+     * @param {string} productId
+     * @param {string|null} colorId
+     * @param {number} qty
+     */
+    addItem(productId, colorId, qty) {
+        const storage = cart.getStorage();
+        const itemIndex = storage.findIndex(
+            (item) => (
+                item.productId === productId && item.colorId === colorId
+            ),
+        );
+
+        if (itemIndex !== -1) {
+            storage[itemIndex].qty += qty;
+        } else {
+            storage.push({
+                productId,
+                colorId,
+                qty,
+            });
+        }
+
+        cart.saveStorage(storage);
+    },
+
+    /**
+     * Removes an item from the shopping cart identified by productId and an optional colorId
+     *
+     * @param {string} productId
+     * @param {string|null} colorId
+     */
+    removeItem(productId, colorId) {
+        const storage = cart.getStorage();
+        cart.saveStorage(
+            storage.filter((item) => (!(item.productId === productId && item.colorId === colorId))),
+        );
+    },
+
+    /**
+     * Updates the qty property on a product in the cart
+     *
+     * @param {string} productId
+     * @param {string|null} colorId
+     * @param {number} qty
+     */
+    updateQty(productId, colorId, qty) {
+        const storage = cart.getStorage();
+
+        const cartItemIndex = storage.findIndex(
+            (item) => (item.productId === productId && item.colorId === colorId),
+        );
+
+        if (cartItemIndex > -1) {
+            storage[cartItemIndex].qty = qty;
+        }
+
+        cart.saveStorage(storage);
+    },
+
+    /**
+     * Clears the shopping cart
+     */
+    clear() {
+        cart.saveStorage([]);
+    },
+
+    /**
+     * Returns the total number of items in our shopping cart
+     *
+     * @return {number}
+     */
+    totalItems() {
+        return cart.getStorage().reduce((acc, item) => (acc + item.qty), 0);
+    },
+
+    /**
+     * Initializes the local storage for our shopping cart
+     */
+    initializeStorage() {
+        if (localStorage.getItem('cart') === null) {
+            localStorage.setItem('cart', '[]');
+        }
+    },
+
+    /**
+     * Gets the local storage as an object
+     *
+     * @returns {array}
+     */
+    getStorage() {
+        cart.initializeStorage();
+        return JSON.parse(localStorage.getItem('cart'));
+    },
+
+    /**
+     * Saves the local storage in its string format
+     *
+     * @param {array} storage
+     */
+    saveStorage(storage) {
+        localStorage.setItem('cart', JSON.stringify(storage));
+    },
+};
+
+export default cart;
