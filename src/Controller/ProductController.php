a8c8626406b449cf55c483ffad952cab6f12aa24
Ryan Weaver <ryan@thatsquality.com>
2020-05-18 16:09:12 -0400

    using param converter

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 
+use App\Entity\Category;
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,7 +22,7 @@ class ProductController extends AbstractController
     /**
      * @Route("/category/{id}", name="app_category")
      */
-    public function showCategory(int $id): Response
+    public function showCategory(Category $category): Response
     {
         return $this->render('product/index.html.twig');
     }
@@ -28,7 +30,7 @@ class ProductController extends AbstractController
     /**
      * @Route("/product/{id}", name="app_product")
      */
-    public function showProduct(int $id): Response
+    public function showProduct(Product $product): Response
     {
         return $this->render('product/index.html.twig');
     }
