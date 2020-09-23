1f4a07be21103dc42628947f850fd0f9d2447d46
shadowc <info@matiasjose.com>
2020-09-23 13:48:33 -0300

    [tuts-apply-diff] Applying existing diff file for step server-validation-checkout-service

---
+++ assets/js/services/checkout-service.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+/**
+ * Makes a POST call to create a purchase object after checkout
+ *
+ * @param {Object} data
+ * @return {Promise}
+ */
+export function createOrder(data) {
+    return axios({
+        method: 'post',
+        url: '/api/purchases',
+        data,
+    });
+}
