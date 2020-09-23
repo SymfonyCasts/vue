83afe5f5ddcea2aae7dcbe4653a1d8dd44750bf9
shadowc <info@matiasjose.com>
2020-09-23 13:48:09 -0300

    [tuts-apply-diff] Applying existing diff file for step single-file-component-create-it

---
+++ assets/js/pages/products.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Hello {{ firstName }}! Is this cooler?</h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            firstName: 'Ryan',
+        };
+    },
+};
+</script>
--- assets/js/products.js
+++ assets/js/products.js
@@ -1,15 +1,9 @@
 import Vue from 'vue';
-
-const template = '<h1>Hello {{ firstName }}! Is this cooler?</h1>';
+import App from './pages/products';
 
 new Vue({
     el: '#app',
-    data() {
-        return {
-            firstName: 'Ryan',
-        };
-    },
     render(h) {
-        return Vue.compile(template).render.call(this, h);
+        return h(App);
     },
 });
