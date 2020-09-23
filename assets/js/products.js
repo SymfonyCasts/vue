28c0b1c1db95b686205165485a701bd5a225ca0a
shadowc <info@matiasjose.com>
2020-09-23 13:48:08 -0300

    [tuts-apply-diff] Applying existing diff file for step vue-instance-add-data

--- assets/js/products.js
+++ assets/js/products.js
@@ -2,5 +2,12 @@ import Vue from 'vue';
 
 const app = new Vue({
     el: '#app',
-    template: '<h1>Hello Vue! Is this cooler?</h1>',
+    data: function() {
+        return {
+            firstName: 'Ryan',
+        };
+    },
+    template: '<h1>Hello {{ firstName }}! Is this cooler?</h1>',
 });
+
+window.app = app;
