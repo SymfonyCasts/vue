9abafddb9087c90a14a9474eff184f2ea2ba4b53
shadowc <info@matiasjose.com>
2020-09-23 13:48:25 -0300

    [tuts-apply-diff] Applying existing diff file for step basic-product-page-created-function

--- assets/js/components/product.vue
+++ assets/js/components/product.vue
@@ -1,10 +1,13 @@
 <template>
     <div>
+        <h1>{{ product.name }}</h1>
         I'd <3 to see a Product here!
     </div>
 </template>
 
 <script>
+import { getProduct } from '@/services/products-service';
+
 export default {
     name: 'Product',
     props: {
@@ -12,6 +15,27 @@ export default {
             type: String,
             default: null,
         },
+    },
+    data() {
+        return {
+            product: null,
+            loading: true,
+        };
+    },
+    async created() {
+        let response;
+
+        try {
+            response = await getProduct(this.currentProductId);
+
+            this.loading = false;
+        } catch (e) {
+            this.loading = false;
+
+            return;
+        }
+
+        this.product = response.data;
     },
 };
 </script>
