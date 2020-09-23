4a67da719a5a59f67b28d94d52e0944a9da4a58e
shadowc <info@matiasjose.com>
2020-09-23 13:48:22 -0300

    [tuts-apply-diff] Applying existing diff file for step helper-add-jsdoc

--- assets/js/helpers/format-price.js
+++ assets/js/helpers/format-price.js
@@ -1,3 +1,9 @@
+/**
+ * Formats a price buy adding a dot and normalizing decimals
+ *
+ * @param {number} price
+ * @returns {string}
+ */
 export default (price) => (
     (price / 100)
         .toLocaleString('en-US', { minimumFractionDigits: 2 })
