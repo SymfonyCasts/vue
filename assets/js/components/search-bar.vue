43d2f035892669367bee21be5c412bc31f55fa3b
shadowc <info@matiasjose.com>
2020-09-23 13:48:20 -0300

    [tuts-apply-diff] Applying existing diff file for step v-model-create-search-component

---
+++ assets/js/components/search-bar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <input
+            class="form-control"
+            placeholder="Search products..."
+            type="search"
+        >
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar',
+};
+</script>
