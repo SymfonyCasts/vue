d13932f156aaa24050b4a7a20154188cecd86f89
shadowc <info@matiasjose.com>
2020-09-23 13:48:31 -0300

    [tuts-apply-diff] Applying existing diff file for step transitions-create-checkout-main-component

---
+++ assets/js/components/checkout/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="row p-3">
+        <div class="col-12">
+            A cool checkout form will appear right here!
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CheckoutForm',
+};
+</script>
