c9e0b6c806c4099a3373549dda1b9fa9ad97e156
shadowc <info@matiasjose.com>
2020-09-23 13:48:24 -0300

    [tuts-apply-diff] Applying existing diff file for step generic-component-add-getcurrentproductid-to-page-context

--- assets/js/services/page-context.js
+++ assets/js/services/page-context.js
@@ -6,3 +6,12 @@
 export function getCurrentCategoryId() {
     return window.currentCategoryId;
 }
+
+/**
+ * Returns the current product id that's set by the server.
+ *
+ * @returns {string|null}
+ */
+export function getCurrentProductId() {
+    return window.currentProductId;
+}
