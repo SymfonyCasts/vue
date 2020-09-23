066e1ff65bae2ee08ef479c9d50748b5a8a11a6b
shadowc <info@matiasjose.com>
2020-09-23 13:48:28 -0300

    [tuts-apply-diff] Applying existing diff file for step shopping-cart-listing-shopping-cart-template

--- assets/js/pages/shopping-cart.vue
+++ assets/js/pages/shopping-cart.vue
@@ -4,17 +4,23 @@
             <aside class="col-xs-12 col-lg-3" />
 
             <div class="col-xs-12 col-lg-9">
-                // TODO: List the products in the cart here
+                <div class="pb-3">
+                    <title-component text="Shopping Cart" />
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import TitleComponent from '@/components/title';
 import shoppingCartMixin from '@/mixins/get-shopping-cart';
 
 export default {
     name: 'ShoppingCart',
+    components: {
+        TitleComponent,
+    },
     mixins: [shoppingCartMixin],
 };
 </script>
