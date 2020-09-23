d4ab5d12e7f61b3c3d8b9a57e9058650802a0c61
shadowc <info@matiasjose.com>
2020-09-23 13:48:24 -0300

    [tuts-apply-diff] Applying existing diff file for step generic-component-add-computed-to-products-vue

--- assets/js/pages/products.vue
+++ assets/js/pages/products.vue
@@ -23,7 +23,7 @@
 <script>
 import Catalog from '@/components/catalog';
 import Sidebar from '@/components/sidebar';
-import { getCurrentCategoryId } from '@/services/page-context';
+import { getCurrentCategoryId, getCurrentProductId } from '@/services/page-context';
 import { fetchCategories } from '@/services/categories-service';
 
 export default {
@@ -45,6 +45,9 @@ export default {
         },
         contentClass() {
             return this.sidebarCollapsed ? 'col-xs-12 col-11' : 'col-xs-12 col-9';
+        },
+        currentProductId() {
+            return getCurrentProductId();
         },
     },
     async created() {
