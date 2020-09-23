13378cae83a4099bc544724fb4e54abb97618e35
shadowc <info@matiasjose.com>
2020-09-23 13:48:15 -0300

    [tuts-apply-diff] Applying existing diff file for step products-listing-paste-v-for-from-catalog

--- assets/js/components/product-list/index.vue
+++ assets/js/components/product-list/index.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="row">
-
+        <div
+            v-for="product in products"
+            :key="product['@id']"
+            class="col-xs-12 col-6 mb-2 pb-2"
+        >
+            {{ product.name }}
+        </div>
     </div>
 </template>
 
