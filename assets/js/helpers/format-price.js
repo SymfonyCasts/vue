e2151c828fb18061e8d7b513ec36c65f1bc12b0b
shadowc <info@matiasjose.com>
2020-09-23 13:48:22 -0300

    [tuts-apply-diff] Applying existing diff file for step helper-use-short-function

--- assets/js/helpers/format-price.js
+++ assets/js/helpers/format-price.js
@@ -1,4 +1,4 @@
-export default (price) => {
-    return (price / 100)
-        .toLocaleString('en-US', { minimumFractionDigits: 2 });
-};
+export default (price) => (
+    (price / 100)
+        .toLocaleString('en-US', { minimumFractionDigits: 2 })
+);
