c16c12c415ac31bc046cbfd6ed712801907ad9a9
shadowc <info@matiasjose.com>
2020-09-23 13:48:16 -0300

    [tuts-apply-diff] Applying existing diff file for step products-listing-add-style

--- assets/js/components/product-list/product-card.vue
+++ assets/js/components/product-list/product-card.vue
@@ -43,3 +43,24 @@ export default {
     },
 };
 </script>
+
+<style lang="scss" module>
+@import '~styles/components/light-component';
+.product-box {
+    border: 1px solid $light-component-border;
+    box-shadow: 0 0 7px 4px #efefee;
+    border-radius: 5px;
+}
+.image {
+    img {
+        width: 100%;
+        height: auto;
+        border-top-left-radius: 5px;
+        border-top-right-radius: 5px;
+    }
+
+    h3 {
+        font-size: 1.2rem;
+    }
+}
+</style>
