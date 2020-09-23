58fc38bb9c3376c60c9283fdf6bc46d6106a7bf3
shadowc <info@matiasjose.com>
2020-09-23 13:48:20 -0300

    [tuts-apply-diff] Applying existing diff file for step title-component-create-component

---
+++ assets/js/components/title.vue
@@ -0,0 +1,21 @@
+<template>
+    <div :class="$style.component">
+        <h1>
+            Products
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Title',
+};
+</script>
+
+<style lang="scss" module>
+.component {
+    h1 {
+        font-size: 1.7rem;
+    }
+}
+</style>
