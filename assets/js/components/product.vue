08bff2cf4329709f4b8c001b36f3335ce56a6975
shadowc <info@matiasjose.com>
2020-09-23 13:48:26 -0300

    [tuts-apply-diff] Applying existing diff file for step basic-product-template-fix-template-after-paste

--- assets/js/components/product.vue
+++ assets/js/components/product.vue
@@ -1,67 +1,69 @@
 <template>
-    <div class="p-3">
-        <title-component :text="product.name" />
-    </div>
-
-    <div
-        :class="$style.product"
-    >
-        <div class="p-3">
-            <loading v-show="loading" />
+    <div>
+        <div class="pb-3">
+            <title-component :text="product.name" />
         </div>
 
-        <div class="row">
-            <div
-                v-show="!loading"
-                class="col-8"
-            >
-                <img
-                    class="d-block"
-                    :src="product.image"
-                    :alt="product.name"
-                >
-
-                <div class="p-2">
-                    <small>brought to you by </small>
-
-                    <small
-                        class="d-inline"
-                        v-text="product.brand"
-                    />
-                </div>
+        <div
+            :class="$style.product"
+        >
+            <div class="p-3">
+                <loading v-show="loading" />
             </div>
 
-            <div
-                v-show="!loading"
-                class="col-4"
-            >
-                <div class="p-3 row">
-                    <p v-text="product.description" />
+            <div class="row">
+                <div
+                    v-show="!loading"
+                    class="col-8"
+                >
+                    <img
+                        class="d-block"
+                        :src="product.image"
+                        :alt="product.name"
+                    >
 
-                    <p>Price: <strong>${{ price }}</strong></p>
+                    <div class="p-2">
+                        <small>brought to you by </small>
+
+                        <small
+                            class="d-inline"
+                            v-text="product.brand"
+                        />
+                    </div>
                 </div>
 
-                <div class="p-3 row align-items-center">
-                    <color-selector
-                        v-show="product.colors.length !== 0"
-                        @color-selected="updateSelectedColor"
-                    />
+                <div
+                    v-show="!loading"
+                    class="col-4"
+                >
+                    <div class="p-3 row">
+                        <p v-text="product.description" />
 
-                    <div class="col-3">
-                        <input
-                            v-model.number="qty"
-                            class="form-control"
-                            type="number"
-                            min="1"
-                        >
+                        <p>Price: <strong>${{ price }}</strong></p>
                     </div>
 
-                    <button
-                        class="btn btn-info btn-sm"
-                        @click="addToCart"
-                    >
-                        Add to Cart
-                    </button>
+                    <div class="p-3 row align-items-center">
+                        <color-selector
+                            v-show="product.colors.length !== 0"
+                            @color-selected="updateSelectedColor"
+                        />
+
+                        <div class="col-3">
+                            <input
+                                v-model.number="qty"
+                                class="form-control"
+                                type="number"
+                                min="1"
+                            >
+                        </div>
+
+                        <button
+                            class="btn btn-info btn-sm"
+                            @click="addToCart"
+                        >
+                            Add to Cart
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
