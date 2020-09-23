0d54f977e3b74046a8dd1061b19b142ec26d24bd
shadowc <info@matiasjose.com>
2020-09-23 13:48:32 -0300

    [tuts-apply-diff] Applying existing diff file for step custom-input-add-form-input-from-checkout

--- assets/js/components/checkout/index.vue
+++ assets/js/components/checkout/index.vue
@@ -2,15 +2,25 @@
     <div class="row p-3">
         <div class="col-12">
             <form>
-
+                <form-input
+                    id="customerName"
+                    v-model="form.customerName"
+                    label="Name:"
+                    :error-message="validationErrors.customerName"
+                />
             </form>
         </div>
     </div>
 </template>
 
 <script>
+import FormInput from '@/components/checkout/form-input';
+
 export default {
     name: 'CheckoutForm',
+    components: {
+        FormInput,
+    },
     props: {
         cart: {
             type: Object,
