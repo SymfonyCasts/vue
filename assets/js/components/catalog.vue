9c0ec8b11be96f56dfdb0a1ad747993216d7fa96
shadowc <info@matiasjose.com>
2020-09-23 13:48:19 -0300

    [tuts-apply-diff] Applying existing diff file for step categories-service-add-products-service

--- assets/js/components/catalog.vue
+++ assets/js/components/catalog.vue
@@ -20,7 +20,7 @@
 </template>
 
 <script>
-import axios from 'axios';
+import { fetchProducts } from '@/services/products-service';
 import LegendComponent from '@/components/legend';
 import ProductList from '@/components/product-list';
 
@@ -44,18 +44,11 @@ export default {
         };
     },
     async created() {
-        const params = {};
-        if (this.currentCategoryId) {
-            params.category = this.currentCategoryId;
-        }
-
         this.loading = true;
 
         let response;
         try {
-            response = await axios.get('/api/products', {
-                params,
-            });
+            response = await fetchProducts(this.currentCategoryId);
 
             this.loading = false;
         } catch (e) {
