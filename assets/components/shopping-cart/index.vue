9e1e6c5bc29035a4ff36bbb73c22c618961de72a
shadowc <info@matiasjose.com>
2021-03-02 13:10:52 -0300

    [tuts-apply-diff] Applying existing diff file for step shopping-cart-ui-extract-to-component

---
+++ assets/components/shopping-cart/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div v-if="items.length === 0">
+            Your cart is empty! Get to shopping!
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ShoppingCartList',
+    props: {
+        items: {
+            type: Array,
+            required: true,
+        },
+    },
+};
+</script>
