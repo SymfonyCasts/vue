7c17aac00e1da836c6e4d09eb2513e20335b05d5
shadowc <info@matiasjose.com>
2021-03-02 13:10:43 -0300

    [tuts-apply-diff] Applying existing diff file for step collapsible-emmit-restructure-sidebar

--- assets/js/components/sidebar.vue
+++ assets/js/components/sidebar.vue
@@ -43,9 +43,14 @@
 <script>
 export default {
     name: 'Sidebar',
+    props: {
+        collapsed: {
+            type: Boolean,
+            required: true,
+        },
+    },
     data() {
         return {
-            collapsed: false,
             categories: [
                 {
                     name: 'Dot Matrix Printers',
