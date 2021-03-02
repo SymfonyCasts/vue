b01bb2846a9494ff5a107771e8b56c3d35a638a0
shadowc <info@matiasjose.com>
2021-03-02 13:10:50 -0300

    [tuts-apply-diff] Applying existing diff file for step add-to-cart-add-loading

--- assets/components/product-show.vue
+++ assets/components/product-show.vue
@@ -54,6 +54,10 @@
                                 @click="addToCart"
                             >
                                 Add to Cart
+                                <i
+                                    v-show="addToCartLoading"
+                                    class="fas fa-spinner fa-spin"
+                                />
                             </button>
                         </div>
                     </div>
@@ -87,6 +91,7 @@ export default {
     data() {
         return {
             cart: null,
+            addToCartLoading: false,
             product: null,
             loading: true,
         };
@@ -112,12 +117,14 @@ export default {
         }
     },
     methods: {
-        addToCart() {
-            addItemToCart(this.cart, {
+        async addToCart() {
+            this.addToCartLoading = true;
+            await addItemToCart(this.cart, {
                 product: this.product['@id'],
                 color: null,
                 quantity: 1,
             });
+            this.addToCartLoading = false;
         },
     },
 };
