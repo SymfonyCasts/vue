3787a82347c671266bc15b8680e3ee6a5fa33bb5
shadowc <info@matiasjose.com>
2021-03-02 13:10:50 -0300

    [tuts-apply-diff] Applying existing diff file for step basic-product-template-paste-product-template

--- assets/components/product-show.vue
+++ assets/components/product-show.vue
@@ -5,6 +5,59 @@
         <div v-if="product">
             <title-component :text="product.name" />
         </div>
+
+        <div
+            v-if="product"
+            :class="$style.product"
+            class="row"
+        >
+            <div class="col-4 pt-3">
+                <img
+                    class="d-block"
+                    :src="product.image"
+                    :alt="product.name"
+                >
+
+                <div class="p-2">
+                    <small>brought to you by </small>
+
+                    <small
+                        class="d-inline"
+                        v-text="product.brand"
+                    />
+                </div>
+            </div>
+
+            <div class="col-8 p-3">
+                <div v-text="product.description" />
+
+                <div class="row mt-4 align-items-center">
+                    <div class="col-4">
+                        Price: <strong>${{ price }}</strong>
+                    </div>
+
+                    <div class="col-8 p-3">
+                        <div class="d-flex align-items-center justify-content-center">
+                            <color-selector
+                                v-if="product.colors.length !== 0"
+                            />
+
+                            <input
+                                class="form-control mx-3"
+                                type="number"
+                                min="1"
+                            >
+
+                            <button
+                                class="btn btn-info btn-sm"
+                            >
+                                Add to Cart
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -40,3 +93,20 @@ export default {
     },
 };
 </script>
+
+<style lang="scss" module>
+@import '~styles/components/light-component';
+
+.product {
+    @include light-component;
+
+    img {
+        max-width:100%;
+        max-height:100%;
+    }
+
+    input {
+        width: 60px;
+    }
+}
+</style>
