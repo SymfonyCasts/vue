c93df6bb2f2ef572d455c074e054d589015688ae
shadowc <info@matiasjose.com>
2021-03-02 13:10:51 -0300

    [tuts-apply-diff] Applying existing diff file for step new-entry-create-main-component

---
+++ assets/pages/shopping-cart.vue
@@ -0,0 +1,36 @@
+<template>
+    <div :class="[$style.component, 'container-fluid']">
+        <div class="row">
+            <aside class="col-xs-12 col-lg-3" />
+
+            <div class="col-xs-12 col-lg-9">
+                <title-component text="Shopping Cart" />
+
+                <div class="content p-3">
+                    TODO - show the cart!
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import TitleComponent from '@/components/title';
+
+export default {
+    name: 'ShoppingCart',
+    components: {
+        TitleComponent,
+    },
+};
+</script>
+
+<style lang="scss" module>
+@import '~styles/components/light-component.scss';
+
+.component :global {
+    .content {
+        @include light-component;
+    }
+}
+</style>
