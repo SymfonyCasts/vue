06ad0303be66bc7b4970c4951c09d92f99ee56d2
shadowc <info@matiasjose.com>
2021-03-02 13:10:47 -0300

    [tuts-apply-diff] Applying existing diff file for step better-search-bar-search-timeout

--- assets/js/components/search-bar.vue
+++ assets/js/components/search-bar.vue
@@ -16,12 +16,18 @@ export default {
     data() {
         return {
             searchTerm: '',
+            searchTimeout: null,
         };
     },
     methods: {
         onInput() {
-            setTimeout(() => {
+            if (this.searchTimeout) {
+                clearTimeout(this.searchTimeout);
+            }
+
+            this.searchTimeout = setTimeout(() => {
                 this.$emit('search-products', { term: this.searchTerm });
+                this.searchTimeout = null;
             }, 200);
         },
     },
