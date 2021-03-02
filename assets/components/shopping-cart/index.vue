94b443c5ed9d4d163c961bd2bdbbe4149f473541
shadowc <info@matiasjose.com>
2021-03-02 13:10:53 -0300

    [tuts-apply-diff] Applying existing diff file for step shopping-cart-ui-products-list

--- assets/components/shopping-cart/index.vue
+++ assets/components/shopping-cart/index.vue
@@ -3,12 +3,23 @@
         <div v-if="items.length === 0">
             Your cart is empty! Get to shopping!
         </div>
+
+        <shopping-cart-item
+            v-for="(item, index) in items"
+            :key="index"
+            :item="item"
+        />
     </div>
 </template>
 
 <script>
+import ShoppingCartItem from '@/components/shopping-cart/cart-item';
+
 export default {
     name: 'ShoppingCartList',
+    components: {
+        ShoppingCartItem,
+    },
     props: {
         items: {
             type: Array,
