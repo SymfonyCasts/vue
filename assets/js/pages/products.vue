d68c125d40ac091f4f7fe004c0bb19958c74731a
shadowc <info@matiasjose.com>
2021-03-02 13:10:43 -0300

    [tuts-apply-diff] Applying existing diff file for step collapsible-emmit-listen-to-emmit

--- assets/js/pages/products.vue
+++ assets/js/pages/products.vue
@@ -2,7 +2,10 @@
     <div class="container-fluid">
         <div class="row">
             <aside class="col-xs-12 col-3">
-                <sidebar :collapsed="sidebarCollapsed" />
+                <sidebar
+                    :collapsed="sidebarCollapsed"
+                    v-on:toggle-collapsed="toggleSidebarCollapsed"
+                />
             </aside>
 
             <div class="col-xs-12 col-9">
@@ -26,6 +29,11 @@ export default {
         return {
             sidebarCollapsed: false,
         };
+    },
+    methods: {
+        toggleSidebarCollapsed() {
+            this.sidebarCollapsed = !this.sidebarCollapsed;
+        },
     },
 };
 </script>
