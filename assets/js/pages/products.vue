d9ee5ece133098b7dc99d816bad81e6e6c7fe7a8
shadowc <info@matiasjose.com>
2021-03-02 13:10:43 -0300

    [tuts-apply-diff] Applying existing diff file for step collapsible-emmit-restructure-products

--- assets/js/pages/products.vue
+++ assets/js/pages/products.vue
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <aside class="col-xs-12 col-3">
-                <sidebar />
+                <sidebar :collapsed="sidebarCollapsed" />
             </aside>
 
             <div class="col-xs-12 col-9">
@@ -21,6 +21,11 @@ export default {
     components: {
         Catalog,
         Sidebar,
+    },
+    data() {
+        return {
+            sidebarCollapsed: false,
+        };
     },
 };
 </script>
