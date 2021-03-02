17c52e56a479aad250bae97bde76bd35c941fc1f
shadowc <info@matiasjose.com>
2021-03-02 13:10:41 -0300

    [tuts-apply-diff] Applying existing diff file for step first-vue-component-sass

--- assets/js/pages/products.vue
+++ assets/js/pages/products.vue
@@ -66,10 +66,10 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+@import '../../scss/components/light-component';
+
 .sidebar {
-    border: 1px solid #efefee;
-    box-shadow: 0px 0px 7px 4px #efefee;
-    border-radius: 5px;
+    @include light-component;
 }
 </style>
