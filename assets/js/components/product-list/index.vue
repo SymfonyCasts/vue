65841edd59908ff37a5a45a99545e56da289a661
shadowc <info@matiasjose.com>
2021-03-02 13:10:44 -0300

    [tuts-apply-diff] Applying existing diff file for step products-listing-first-product-list

---
+++ assets/js/components/product-list/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="row">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductList',
+    props: {
+        products: {
+            type: Array,
+            required: true,
+        },
+    },
+};
+</script>
