53f102279738bb69ade408debc3df4bb872a4000
shadowc <info@matiasjose.com>
2021-03-02 13:10:56 -0300

    [tuts-apply-diff] Applying existing diff file for step checkout-form-basic-data

--- assets/components/checkout/index.vue
+++ assets/components/checkout/index.vue
@@ -9,5 +9,17 @@
 <script>
 export default {
     name: 'CheckoutForm',
+    data() {
+        return {
+            form: {
+                customerName: '',
+                customerEmail: '',
+                customerAddress: '',
+                customerZip: '',
+                customerCity: '',
+                customerPhone: '',
+            },
+        };
+    },
 };
 </script>
