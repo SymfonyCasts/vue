279bdce2ad9042b521fc32d1ddc6b12da83b2aa2
shadowc <info@matiasjose.com>
2021-03-02 13:10:53 -0300

    [tuts-apply-diff] Applying existing diff file for step shopping-cart-ui-add-cart-structure

--- assets/components/shopping-cart/index.vue
+++ assets/components/shopping-cart/index.vue
@@ -4,11 +4,30 @@
             Your cart is empty! Get to shopping!
         </div>
 
-        <shopping-cart-item
-            v-for="item in items"
-            :key="item.id"
-            :item="item"
-        />
+        <div v-if="items.length">
+            <div class="row p-3">
+                <div class="col-3">
+                    Item Name
+                </div>
+                <div class="col-3">
+                    Quantity
+                </div>
+                <div class="col-3">
+                    Price
+                </div>
+                <div class="col-3" />
+            </div>
+
+            <shopping-cart-item
+                v-for="item in items"
+                :key="item.id"
+                :item="item"
+            />
+
+            <div class="p-3">
+                Total: <strong>$ // TODO</strong>
+            </div>
+        </div>
     </div>
 </template>
 
