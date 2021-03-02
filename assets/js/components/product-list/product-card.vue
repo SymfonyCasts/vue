64b3002d71d39786b2fad0c8213889c150054b3e
shadowc <info@matiasjose.com>
2021-03-02 13:10:44 -0300

    [tuts-apply-diff] Applying existing diff file for step products-listing-paste-product-card-template

--- assets/js/components/product-list/product-card.vue
+++ assets/js/components/product-list/product-card.vue
@@ -1,6 +1,34 @@
 <template>
     <div class="col-xs-12 col-6 mb-2 pb-2">
-        {{ product.name }}
+        <div :class="$style['product-box']">
+            <div :class="$style.image">
+                <img
+                    :alt="product.name"
+                    :src="product.image"
+                    class="d-block mb-2"
+                >
+
+                <h3 class="font-weight-bold mb-2 px-2">
+                    {{ product.name }}
+                </h3>
+            </div>
+
+            <div class="p-2 my-3 d-md-flex justify-content-between">
+                <p class="p-0 d-inline">
+                    <strong>${{ product.price }}</strong>
+                </p>
+
+                <button
+                    class="btn btn-info btn-sm"
+                >
+                    View Product
+                </button>
+            </div>
+        </div>
+        <hr>
+        <div class="px-2 pb-2">
+            <small>brought to you by {{ product.brand }}</small>
+        </div>
     </div>
 </template>
 
