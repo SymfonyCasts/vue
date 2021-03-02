6c326c0dcc5b291296966625db38f50eafff81d9
shadowc <info@matiasjose.com>
2021-03-02 13:10:44 -0300

    [tuts-apply-diff] Applying existing diff file for step products-listing-create-product-card

---
+++ assets/js/components/product-list/product-card.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="col-xs-12 col-6 mb-2 pb-2">
+        {{ product.name }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductCard',
+    props: {
+        product: {
+            type: Object,
+            required: true,
+        },
+    },
+};
+</script>
