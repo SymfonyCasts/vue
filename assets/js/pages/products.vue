64056b9c5728eae336cfc1680eff3f4b819123e2
shadowc <info@matiasjose.com>
2021-03-02 13:10:47 -0300

    [tuts-apply-diff] Applying existing diff file for step watcher-currentcategoryid-to-data

--- assets/js/pages/products.vue
+++ assets/js/pages/products.vue
@@ -36,6 +36,7 @@ export default {
         return {
             sidebarCollapsed: false,
             categories: [],
+            currentCategoryId: getCurrentCategoryId(),
         };
     },
     computed: {
@@ -44,9 +45,6 @@ export default {
         },
         contentClass() {
             return this.sidebarCollapsed ? 'col-xs-12 col-11' : 'col-xs-12 col-9';
-        },
-        currentCategoryId() {
-            return getCurrentCategoryId();
         },
     },
     async created() {
