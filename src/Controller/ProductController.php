1ae070f5f934c155a9412b9b6da5a301a1f397a2
shadowc <info@matiasjose.com>
2021-03-02 13:10:46 -0300

    [tuts-apply-diff] Applying existing diff file for step optimized-load-times-add-categoriesjson-to-controller

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller;
 
-
 use ApiPlatform\Core\Api\IriConverterInterface;
 use App\Entity\Category;
 use App\Entity\Product;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,18 +15,21 @@ class ProductController extends AbstractController
     /**
      * @Route("/", name="app_homepage")
      */
-    public function index(): Response
+    public function index(CategoryRepository $categoryRepository): Response
     {
-        return $this->render('product/index.html.twig');
+        return $this->render('product/index.html.twig', [
+            'categories' => $categoryRepository->findAll(),
+        ]);
     }
 
     /**
      * @Route("/category/{id}", name="app_category")
      */
-    public function showCategory(Category $category, IriConverterInterface $iriConverter): Response
+    public function showCategory(Category $category, IriConverterInterface $iriConverter, CategoryRepository $categoryRepository): Response
     {
         return $this->render('product/index.html.twig', [
             'currentCategoryId' => $iriConverter->getIriFromItem($category),
+            'categories' => $categoryRepository->findAll(),
         ]);
     }
 
