20434b84715985609f7576624ffe7aa2cc6d4b44
shadowc <info@matiasjose.com>
2021-03-02 13:10:57 -0300

    [tuts-apply-diff] Applying existing diff file for step server-validation-submit-prevent

--- assets/components/checkout/index.vue
+++ assets/components/checkout/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row p-3">
         <div class="col-12">
-            <form @submit="onSubmit">
+            <form @submit.prevent="onSubmit">
                 <div class="form-row">
                     <form-input
                         v-model="form.customerName"
@@ -106,8 +106,7 @@ export default {
                 errorMessage: this.validationErrors[id],
             };
         },
-        async onSubmit(event) {
-            event.preventDefault();
+        async onSubmit() {
             this.loading = true;
 
             try {
