73af80b59615da906726a05f9066fac8938c7c8e
shadowc <info@matiasjose.com>
2021-03-02 13:10:41 -0300

    [tuts-apply-diff] Applying existing diff file for step child-component-add-legend-vue

---
+++ assets/js/components/legend.vue
@@ -0,0 +1,11 @@
+<template>
+    <span class="p-3">
+        Shipping takes 10-12 weeks, and products probably won't work
+    </span>
+</template>
+
+<script>
+export default {
+    name: 'Legend',
+};
+</script>
