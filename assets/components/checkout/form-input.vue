1f0c5d63a58809d5940f6633fd7dcfe44f6677ad
shadowc <info@matiasjose.com>
2021-03-02 13:10:56 -0300

    [tuts-apply-diff] Applying existing diff file for step custom-input-extract-into-form-field-component

---
+++ assets/components/checkout/form-input.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="form-group">
+        <label
+            for="customerName"
+            class="col-form-label"
+        >
+            Name:
+        </label>
+        <input
+            id="customerName"
+            v-model.trim="form.customerName"
+            type="text"
+            :class="{
+                'is-invalid': !isFieldValid('customerName'),
+                'form-control': true,
+            }"
+        >
+        <span
+            v-show="!isFieldValid('customerName')"
+            class="invalid-feedback"
+        >
+            {{ validationErrors.customerName }}
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FormInput',
+    methods: {
+        isFieldValid(fieldName) {
+            return !(fieldName in this.validationErrors);
+        },
+    },
+};
+</script>
--- assets/components/checkout/index.vue
+++ assets/components/checkout/index.vue
@@ -2,29 +2,6 @@
     <div class="row p-3">
         <div class="col-12">
             <form>
-                <div class="form-group">
-                    <label
-                        for="customerName"
-                        class="col-form-label"
-                    >
-                        Name:
-                    </label>
-                    <input
-                        id="customerName"
-                        v-model.trim="form.customerName"
-                        type="text"
-                        :class="{
-                            'is-invalid': !isFieldValid('customerName'),
-                            'form-control': true,
-                        }"
-                    >
-                    <span
-                        v-show="!isFieldValid('customerName')"
-                        class="invalid-feedback"
-                    >
-                        {{ validationErrors.customerName }}
-                    </span>
-                </div>
             </form>
         </div>
     </div>
@@ -45,11 +22,6 @@ export default {
             },
             validationErrors: {},
         };
-    },
-    methods: {
-        isFieldValid(fieldName) {
-            return !(fieldName in this.validationErrors);
-        },
     },
 };
 </script>
