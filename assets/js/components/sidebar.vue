d7804fb0890d2743380387b859ccefa2f254243b
shadowc <info@matiasjose.com>
2021-03-02 13:10:42 -0300

    [tuts-apply-diff] Applying existing diff file for step sidebar-create-the-sidebar-component

---
+++ assets/js/components/sidebar.vue
@@ -0,0 +1,48 @@
+<template>
+    <div :class="[$style.sidebar, 'p-3', 'mb-5']">
+        <h5 class="text-center">
+            Categories
+        </h5>
+
+        <ul class="nav flex-column mb4">
+            <li class="nav-item">
+                <a
+                    class="nav-link"
+                    href="/"
+                >All Products</a>
+            </li>
+            <li class="nav-item">
+                <a
+                    class="nav-link"
+                    href="#"
+                >Category A</a>
+            </li>
+            <li class="nav-item">
+                <a
+                    class="nav-link"
+                    href="#"
+                >Category B</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Sidebar',
+};
+</script>
+
+<style lang="scss" module>
+@import '../../scss/components/light-component';
+
+.sidebar {
+    @include light-component;
+
+    ul {
+        li a:hover {
+            background: $blue-component-link-hover;
+        }
+    }
+}
+</style>
