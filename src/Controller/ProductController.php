8b25e863ca860c3d950258e4d3d248e7a37c41e7
shadowc <info@matiasjose.com>
2021-03-02 13:10:49 -0300

    [tuts-apply-diff] Applying existing diff file for step linking-products-add-currentproductid

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -36,10 +36,11 @@ class ProductController extends AbstractController
     /**
      * @Route("/product/{id}", name="app_product")
      */
-    public function showProduct(Product $product, CategoryRepository $categoryRepository): Response
+    public function showProduct(Product $product, CategoryRepository $categoryRepository, IriConverterInterface $iriConverter): Response
     {
         return $this->render('product/index.html.twig', [
             'categories' => $categoryRepository->findAll(),
+            'currentProductId' => $iriConverter->getIriFromItem($product),
         ]);
     }
 }
