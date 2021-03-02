78239a56462e249fcf549517170d35f7c7ab5056
shadowc <info@matiasjose.com>
2021-03-02 13:10:58 -0300

    [tuts-apply-diff] Applying existing diff file for step client-validation-log-validationerrors

--- assets/components/checkout/index.vue
+++ assets/components/checkout/index.vue
@@ -159,6 +159,8 @@ export default {
             }
         },
         validateField(event) {
+            console.log(this.validationErrors);
+
             const validationMessages = {
                 customerName: 'Please, enter your full name!',
                 customerEmail: 'Please, enter your email address!',
