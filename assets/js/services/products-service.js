32bfcb3d026e214c93437fb222715241c855072d
shadowc <info@matiasjose.com>
2021-03-02 13:10:47 -0300

    [tuts-apply-diff] Applying existing diff file for step better-search-bar-adding-search-to-fetchproducts

--- assets/js/services/products-service.js
+++ assets/js/services/products-service.js
@@ -2,12 +2,17 @@ import axios from 'axios';
 
 /**
  * @param {string|null} categoryIri
+ * @param {string|null} searchTerm
  * @returns {Promise}
  */
-export function fetchProducts(categoryIri) {
+export function fetchProducts(categoryIri, searchTerm) {
     const params = {};
     if (categoryIri) {
         params.category = categoryIri;
+    }
+
+    if (searchTerm) {
+        params.name = searchTerm;
     }
 
     return axios.get('/api/products', {
