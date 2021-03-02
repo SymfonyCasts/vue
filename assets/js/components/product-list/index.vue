3e179f776564c2e9d2586f1e1c4efaf08ec107f1
shadowc <info@matiasjose.com>
2021-03-02 13:10:44 -0300

    [tuts-apply-diff] Applying existing diff file for step loading-state-product-list-template

--- assets/js/components/product-list/index.vue
+++ assets/js/components/product-list/index.vue
@@ -1,7 +1,14 @@
 <template>
     <div class="row">
+        <div class="col-12">
+            <div class="mt-4">
+                <loading v-show="products.length === 0" />
+            </div>
+        </div>
+
         <product-card
             v-for="product in products"
+            v-show="products.length > 0"
             :key="product['@id']"
             :product="product"
         />
