cfbe93f7cac4b02175aa1ff8d186f8c0e9c41310
shadowc <info@matiasjose.com>
2021-03-02 13:10:50 -0300

    [tuts-apply-diff] Applying existing diff file for step add-to-cart-add-quantity

--- assets/components/product-show.vue
+++ assets/components/product-show.vue
@@ -43,6 +43,7 @@
                             />
 
                             <input
+                                v-model.number="quantity"
                                 class="form-control mx-3"
                                 type="number"
                                 min="1"
@@ -95,6 +96,7 @@ export default {
     data() {
         return {
             cart: null,
+            quantity: 1,
             addToCartLoading: false,
             addToCartSuccess: false,
             product: null,
@@ -128,7 +130,7 @@ export default {
             await addItemToCart(this.cart, {
                 product: this.product['@id'],
                 color: null,
-                quantity: 1,
+                quantity: this.quantity,
             });
             this.addToCartLoading = false;
             this.addToCartSuccess = true;
