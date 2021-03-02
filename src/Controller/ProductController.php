8b7a2c21773683dbdb29e75b471a314a34f51be4
shadowc <info@matiasjose.com>
2021-03-02 13:10:45 -0300

    [tuts-apply-diff] Applying existing diff file for step categories-filter-controller-variable

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use ApiPlatform\Core\Api\IriConverterInterface;
 use App\Entity\Category;
 use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,9 +23,11 @@ class ProductController extends AbstractController
     /**
      * @Route("/category/{id}", name="app_category")
      */
-    public function showCategory(Category $category): Response
+    public function showCategory(Category $category, IriConverterInterface $iriConverter): Response
     {
-        return $this->render('product/index.html.twig');
+        return $this->render('product/index.html.twig', [
+            'currentCategoryId' => $iriConverter->getIriFromItem($category),
+        ]);
     }
 
     /**
