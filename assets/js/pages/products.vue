968d69f2ff3683c0b800bf4f9cee32fc1c52f5a7
shadowc <info@matiasjose.com>
2021-03-02 13:10:41 -0300

    [tuts-apply-diff] Applying existing diff file for step first-vue-component-add-styles

--- assets/js/pages/products.vue
+++ assets/js/pages/products.vue
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <aside class="col-xs-12 col-3">
-                <div class="p-3 mb-5">
+                <div class="sidebar p-3 mb-5">
                     <h5 class="text-center">
                         Categories
                     </h5>
@@ -65,3 +65,11 @@ export default {
     },
 };
 </script>
+
+<style>
+.sidebar {
+    border: 1px solid #efefee;
+    box-shadow: 0px 0px 7px 4px #efefee;
+    border-radius: 5px;
+}
+</style>
