59cb0b19a414c1cdfe64697d176bd6be83b13bdd
shadowc <info@matiasjose.com>
2021-03-02 13:10:57 -0300

    [tuts-apply-diff] Applying existing diff file for step server-validation-adding-submit

--- assets/components/checkout/index.vue
+++ assets/components/checkout/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row p-3">
         <div class="col-12">
-            <form>
+            <form @submit="onSubmit">
                 <div class="form-row">
                     <form-input
                         v-model="form.customerName"
