f5cc979c039596f427610e54786e1db9f83d0413
shadowc <info@matiasjose.com>
2021-03-02 13:10:53 -0300

    [tuts-apply-diff] Applying existing diff file for step more-shopping-cart-cast-to-int

--- assets/components/shopping-cart/cart-item.vue
+++ assets/components/shopping-cart/cart-item.vue
@@ -59,7 +59,7 @@ export default {
             this.$emit('updateQuantity', {
                 productId: this.item.product['@id'],
                 colorId: this.item.color ? this.item.color['@id'] : null,
-                quantity: event.target.value,
+                quantity: parseFloat(event.target.value),
             });
         },
     },
