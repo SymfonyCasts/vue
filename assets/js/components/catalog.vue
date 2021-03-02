e3e452dc16afcf87f9f8af2f2b3f0a80bdc9ec77
shadowc <info@matiasjose.com>
2021-03-02 13:10:42 -0300

    [tuts-apply-diff] Applying existing diff file for step sidebar-create-the-catalog-component

---
+++ assets/js/components/catalog.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-12">
+                <h1>
+                    Products
+                </h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-12 col-6 mb-2 pb-2">
+                TODO - load some products!
+            </div>
+        </div>
+
+        <div class="row">
+            <legend-component :title="legend" />
+        </div>
+    </div>
+</template>
+
+<script>
+import LegendComponent from './legend';
+
+export default {
+    name: 'Catalog',
+    components: {
+        LegendComponent,
+    },
+    data() {
+        return {
+            legend: 'Shipping takes 10-13 weeks, and products probably won\'t work',
+        };
+    },
+};
+</script>
